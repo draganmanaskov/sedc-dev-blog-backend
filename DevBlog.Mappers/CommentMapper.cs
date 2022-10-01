cf0073447ee1d514aee4b509a9eae86a0b3334a9
Vlatko Nikolovski <nikolovski.vlatko97@gmail.com>
2022-10-01 13:31:05 +0200

    Comment service almost done

---
+++ DevBlog.Mappers/CommentMapper.cs
@@ -0,0 +1,23 @@
+﻿using DevBlog.Domain.Models;
+using DevBlog.Dtos.Comments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.Mappers
+{
+    public static class CommentMapper
+    {
+        public static Comment ToComment(this CreateCommentDto createCommentDto)
+        {
+            return new Comment
+            {
+                Body = createCommentDto.Body,
+                PostId = createCommentDto.PostId,
+                UserId = createCommentDto.UserId == 0 ? null : createCommentDto.UserId,
+            };
+        }
+    }
+}
--- DevBlog.Services/Implementations/CommentService.cs
+++ DevBlog.Services/Implementations/CommentService.cs
@@ -1,6 +1,9 @@
-﻿using DevBlog.DataAccess.Interfaces;
+﻿using DevBlog.DataAccess.Implementations;
+using DevBlog.DataAccess.Interfaces;
 using DevBlog.Dtos.Comments;
+using DevBlog.Mappers;
 using DevBlog.Services.Interfaces;
+using DevBlog.Shared.CustomExceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +15,57 @@ namespace DevBlog.Services.Implementations
     public class CommentService : ICommentService
     {
         private readonly ICommentRepository _commentRepository;
-        public CommentService(ICommentRepository commentRepository)
+        private readonly IPostRepository _postRepository;
+        private readonly IUserRepository _userRepository;
+        public CommentService(ICommentRepository commentRepository, IPostRepository postRepository, IUserRepository userRepository)
         {
             _commentRepository = commentRepository;
+            _postRepository = postRepository;
+            _userRepository = userRepository;
         }
 
         public void CreateComment(CreateCommentDto createCommentDto)
         {
-            throw new NotImplementedException();
+            var post = _postRepository.GetById(createCommentDto.PostId);
+            if (post == null)
+            {
+                throw new PostNotFoundException($"Post with id {createCommentDto.PostId} does not exist!");
+            }
+            var user = _userRepository.GetById(createCommentDto.UserId);
+            if (user == null && createCommentDto.UserId != 0)
+            {
+                throw new UserNotFoundException("User does not exist!");
+            }
+            _commentRepository.Add(createCommentDto.ToComment());
         }
 
-        public void DeleteComment(int id)
+        public void DeleteComment(int id, int loggedInUserId)
         {
-            throw new NotImplementedException();
+            var comment = _commentRepository.GetById(id);
+            if (comment == null)
+            {
+                throw new NotImplementedException(); // replace with CommentNotFoundException
+            }
+            if (comment.UserId != loggedInUserId)
+            {
+                throw new NotImplementedException(); // replace with UnauthorizedUserException
+            }
+            _commentRepository.Delete(comment);
         }
 
-        public void UpdateComment(UpdateCommentDto updateCommentDto)
+        public void UpdateComment(UpdateCommentDto updateCommentDto, int loggedInUserId)
         {
-            throw new NotImplementedException();
+            var comment = _commentRepository.GetById(updateCommentDto.Id);
+            if (comment == null)
+            {
+                throw new NotImplementedException(); // replace with CommentNotFoundException
+            }
+            if (comment.UserId != loggedInUserId)
+            {
+                throw new NotImplementedException(); // replace with UnauthorizedUserException
+            }
+            comment.Body = updateCommentDto.Body;
+            _commentRepository.Update(comment);
         }
     }
 }
